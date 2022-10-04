2f8254483b2718d398bed2c3cb5781c1a949a2a2
Zahon Matthews <zaematthews@gmail.com>
2022-10-03 21:02:44 -0400

    almost done testtask2

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 -- Part 1: Test it with SQL
-
+--CREATE TABLE job(
+--id INT,
+--employer VARCHAR(255),
+--name VARCHAR(255),
+--skills VARCHAR(255),
 -- Part 2: Test it with SQL
-
+-- SELECT name FROM techjobs.employer;
 -- Part 3: Test it with SQL
 
 -- Part 4: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,41 @@
 package org.launchcode.techjobs.persistent.models;
 
-public class Skill extends AbstractEntity {
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+public class Skill extends AbstractEntity {
+    @NotNull
+    @Size(min = 3, max = 300, message = "Description should be less than 3000 characters.")
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+
+    public Skill() {}
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    //    Do we need to be able to add jobs?
+//    public void addJob(Job job) {
+//        this.jobs.add(job);
+//    }
 }
